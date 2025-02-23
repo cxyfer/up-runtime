709961730f41428da231f6a6a4999248aca4cf03
cxyfer <gdst.tw@gmail.com>
2025-02-24 00:43:28 +0800

    Add start.bat script for launching Docker runtime container

---
+++ start.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+REM Use fixed username and password
+set "_USER=upclass"
+set "_PASS=password"
+
+set _SSHPORT=22222
+
+REM Get script directory
+set "_DIR=%~dp0"
+set "_DIR=!_DIR:~0,-1!"
+
+echo ** Using user: !_USER!; password = !_PASS!
+if not exist "!_DIR!\home" mkdir "!_DIR!\home"
+
+REM Run docker container
+docker run -d --name up-runtime --restart unless-stopped ^
+    --cap-add SYS_ADMIN --cap-add NET_ADMIN --cap-add DAC_READ_SEARCH ^
+    -v "!_DIR!\home:/home" -p "!_SSHPORT!:22/tcp" ^
+    upclass/runtime /entrypoint.sh "!_USER!" "!_PASS!" "1000" "1000"
+endlocal
