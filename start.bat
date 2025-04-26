1c879c553983753c79c58a99debe2142c55c2f0a
cxyfer <gdst.tw@gmail.com>
2025-04-26 16:34:27 +0800

    🔧 chore: Grant privileged access to the container
    Adds the `--privileged` flag to the `docker run` command.
    
    This allows the container to perform certain operations that would otherwise be restricted, potentially resolving permission issues or enabling specific functionalities within the container.

--- start.bat
+++ start.bat
@@ -16,7 +16,7 @@ if not exist "!_DIR!\home" mkdir "!_DIR!\home"
 
 REM Run docker container
 docker run -d --name up-runtime --restart unless-stopped ^
+    --privileged ^
     --cap-add SYS_ADMIN --cap-add NET_ADMIN --cap-add DAC_READ_SEARCH ^
     -v "!_DIR!\home:/home" -p "!_SSHPORT!:22/tcp" ^
     upclass/runtime /entrypoint.sh "!_USER!" "!_PASS!" "1000" "1000"
-endlocal
